7291e6e59df6d596ddcfbc9f7ef18b9b6d79e5de
Cai <cyqmailbox@gmail.com>
2016-10-03 18:37:00 +0900

    complete fastSortString and binary search by iterator

--- chapter_3/iterator/Example_binarySearch/search.cpp
+++ chapter_3/iterator/Example_binarySearch/search.cpp
@@ -5,7 +5,7 @@
 #include <fstream>
 using namespace std;
 
-void fastSortString(vector<string> &strVector, auto begin, auto end);
+void fastSortString(vector<string>::iterator begin, vector<string>::iterator end);
 
 int main(int argc, char **argv)
 {
@@ -15,10 +15,56 @@ int main(int argc, char **argv)
 
     while(file >> temp)
 	strVector.push_back(temp);
-    fastSortString(strVector, strVector.begin(), strVector.end());
+    fastSortString(strVector.begin(), strVector.end());
+    for(auto s: strVector)
+	cout << s << ' ';
+    cout << endl;
+    string target = argv[2];
+    auto begin = strVector.begin(), end = strVector.end();
+    auto mid = begin + (end - begin) / 2;
+    while(mid != end)
+    {
+	if(*mid == target)
+	{
+	    cout << target << " is existing\n";
+	    return 0;
+	}
+	if(*mid > target)
+	    end = mid;
+	else
+	    begin = mid + 1;
+	mid = begin + (end - begin) / 2;
+    }
+    cout << "can't find " << target << endl;
+    return 0;
 }
 
-void fastSortString(vector<string> &strVector, auto begin, auto end)
-{
-    
+void swap(vector<string>::iterator a, vector<string>::iterator b)
+{    
+    string temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
 }
+
+void fastSortString(vector<string>::iterator begin, vector<string>::iterator end)
+{
+    if(begin >= end)
+	return;
+    auto smallEndIter = begin;
+    for(auto i = begin + 1; i < end; ++i)
+	if(*i <= *begin)
+	    swap(i, ++smallEndIter);
+    swap(begin, smallEndIter);
+    fastSortString(begin, smallEndIter);
+    fastSortString(smallEndIter + 1, end);
+}
+
+
+
+
+
+
+
+
+
